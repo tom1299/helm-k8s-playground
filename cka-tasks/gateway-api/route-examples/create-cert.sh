def3fe28d0f16ea375afd63ef5a736d4ea3d6269
tom1299 <tom1299@jeeatwork.com>
2025-08-03 12:52:40 +0200

    Add example for tls termination

--- cka-tasks/gateway-api/route-examples/create-cert.sh
+++ cka-tasks/gateway-api/route-examples/create-cert.sh
@@ -4,7 +4,7 @@ rm example-4.com.key example-4.com.csr example-4.com.crt
 openssl genrsa -out example-4.com.key 2048
 
 openssl req -new -key example-4.com.key -out example-4.com.csr \
-  -subj "/CN=example.com"
+  -subj "/CN=example4.com"
 
 openssl x509 -req -in example-4.com.csr -signkey example-4.com.key \
   -out example-4.com.crt -days 365
