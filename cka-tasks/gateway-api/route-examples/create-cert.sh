7bae147941f06d41ac4a36f2187d936800a0123b
tom1299 <tom1299@jeeatwork.com>
2025-08-03 08:05:32 +0200

    Add tls secret cert and key for example 4

---
+++ cka-tasks/gateway-api/route-examples/create-cert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm example-4.com.key example-4.com.csr example-4.com.crt
+
+openssl genrsa -out example-4.com.key 2048
+
+openssl req -new -key example-4.com.key -out example-4.com.csr \
+  -subj "/CN=example.com"
+
+openssl x509 -req -in example-4.com.csr -signkey example-4.com.key \
+  -out example-4.com.crt -days 365
+
+kubectl create secret tls example-4-tls-secret --key=example-4.com.key --cert=example-4.com.crt
